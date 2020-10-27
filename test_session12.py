f70724c7e4392a11b8ff519c300acd6cce7ec95d
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-10-27 22:52:02 +0530

    Create test_session12.py

---
+++ test_session12.py
@@ -0,0 +1,33 @@
+import pytest, os
+import calculator
+
+
+#1
+def test_package_set():
+    assert calculator.__package__ == 'calculator'
+
+
+#2
+def test_calc_all():
+    assert calculator.__all__ == [['cos_', 'exp_', 'log_', 'sin_', 'tan_', 'relu_', 'sigmoid_']]
+
+
+#3
+def test_calc_name():
+    assert calculator.__name__ == 'calculator'
+
+
+#4
+def test_calc_file():
+    assert '__init__.py' in calculator.__file__
+
+
+#5
+def test_calc_path():
+    assert type(calculator.__path__) == type([3,4])
+
+
+
+#6
+def test_calc_func():
+    assert calculator.cos_(6)
