cb3286ec6185df927923d318a1a363134c31dde4
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-10-27 12:12:05 +0530

    Add files via upload

--- calculator/derivatives/__init__.py
+++ calculator/derivatives/__init__.py
@@ -1,1 +1,10 @@
+# sub-package __init__.py
 
+from calculator.cos  import cos_derivative
+from calculator.exp  import exp_derivative  
+from calculator.log  import log_derivative
+from calculator.relu import relu_derivative
+from calculator.sigmoid import sigmoid_derivative
+from calculator.sin  import sin_derivative
+from calculator.tan  import tan_derivative
+from calculator.tanh import tanh_derivative
